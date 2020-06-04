6f286ffdc7ca717f8d2bfb6519c2ff2314c7d524
lepasq <pasquarelli.le@gmail.com>
2020-06-04 12:41:47 +0200

    Replace polybar with fork

---
+++ .config/polybar/scripts/weather.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import urllib
+import urllib.parse
+import urllib.request
+import os
+
+
+def main():
+    city = "Ulm"
+    api_key = "api_key"
+
+    try:
+        data = urllib.parse.urlencode({'q': city, 'appid': api_key, 'units': 'metric'})
+        weather = json.loads(urllib.request.urlopen(
+            'http://api.openweathermap.org/data/2.5/weather?' + data)
+            .read())
+        desc = weather['weather'][0]['description'].capitalize()
+        temp = int(float(weather['main']['temp']))
+        #return '{}, {}°F'.format(desc, temp)
+        return '{}°C'.format(temp)
+    except:
+        return ''
+
+
+if __name__ == "__main__":
+	print(main())
