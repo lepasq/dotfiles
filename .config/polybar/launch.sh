12dbc8c8685a17e38e02689965d7c2146e755bab
lepasq <pasquarelli.le@gmail.com>
2020-07-21 22:56:01 +0200

    Make polybar ready for multiple monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
 
+# Kill running polybar
 killall -q polybar
+
+# Wait until killing is finished
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
 polybar main
+
+
+# Launch on multiple monitors
+
+#if type "xrandr"; then
+#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#    MONITOR=$m polybar --reload main &
+#  done
+#else
+#  polybar --reload main &
+#fi
