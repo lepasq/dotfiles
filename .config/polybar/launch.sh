8de97ca90ab297643fd3d7d865e4f06657d97fec
lepasq <pasquarelli.le@gmail.com>
2020-11-11 16:13:19 +0100

    Replace custom polybar with default polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bars
-polybar DP-2-1 &
-polybar eDP-1 &
+polybar -c $HOME/.config/polybar/config.ini DP-2-1 &
+polybar -c $HOME/.config/polybar/config.ini eDP-1 &
