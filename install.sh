9f0b2a5f5e1e8fa172cb3ffdf20256dedc58fa9e
lepasq <pasquarelli.le@gmail.com>
2020-11-13 23:50:09 +0100

    Add readme file

--- install.sh
+++ install.sh
@@ -4,14 +4,6 @@ cd dotfiles
 path=$(pwd)
 
 
-# Home directory
-ln -sf $path/.gitconfig $HOME/.gitconfig
-ln -sf $path/.tmux.conf $HOME/.tmux.conf
-ln -sf $path/.xprofile $HOME/.xprofile
-ln -sf $path/.zprofile $HOME/.zprofile
-ln -sf $path/.zshrc $HOME/.zshrc
-
-
 # Config directory
 [ ! -d $HOME/.config ] && mkdir $HOME/.config
 
@@ -46,6 +38,15 @@ ln -sf $path/.config/wallpaper $HOME/.config/wallpaper
 ln -sf $path/.config/zathura/ $HOME/.config/zathura/
 
 ln -sf $path/.config/aliasrc $HOME/.config/aliasrc
+source $HOME/.config/aliasrc
+
+
+# Home directory
+ln -sf $path/.gitconfig $HOME/.gitconfig
+ln -sf $path/.tmux.conf $HOME/.tmux.conf
+ln -sf $path/.xprofile $HOME/.xprofile
+ln -sf $path/.zprofile $HOME/.zprofile
+ln -sf $path/.zshrc $HOME/.zshrc
 
 
 # Local directory
@@ -53,3 +54,24 @@ ln -sf $path/.config/aliasrc $HOME/.config/aliasrc
 
 [ -d $HOME/.local/bin ] && rm -r $HOME/.local/bin
 ln -sf $path/.local/bin $HOME/.local/bin
+
+
+# Install Vim-Plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Install oh-my-zsh and zsh plugins
+chsh -s /usr/bin/zsh # Change default shell to zsh for current user
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Gitconfig
+echo "Please enter your GitHub username."
+read GIT_USER
+echo "Please enter your GitHub email address."
+read GIT_MAIL
+
+git config --global user.name "$(GIT_USER)"
+git config --global user.email "$(GIT_MAIL)"
